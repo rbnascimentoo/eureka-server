7867a5477768e66737a172dc2b28a07b594e9aa7
Rafael <rnascimentoo@live.com>
2019-12-05 08:55:41 -0300

    first commit

--- src/main/java/br/com/rnascimento/eurekaServer/EurekaServerApplication.java
+++ src/main/java/br/com/rnascimento/eurekaServer/EurekaServerApplication.java
@@ -1,5 +1,7 @@
 package br.com.rnascimento.eurekaServer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,8 +9,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+	
+	private static final Logger LOG = LogManager.getLogger();
 
 	public static void main(String[] args) {
+		LOG.info("### ...Iniciando o serviço EUREKA-SERVER... ###");
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
